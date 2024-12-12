ce1e605368012e39b6bdac81ac446859fc05c25e
moslem <moslemamiri82@gmail.com>
2024-12-12 12:35:44 +0330

    changed database from sqlight to mysql

--- config/settings.py
+++ config/settings.py
@@ -86,8 +86,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("DATABASE_NAME"),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
     }
 }
 
